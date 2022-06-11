40ecd0c3dab9682a56f3e635cf5bca7fcc4b8464
Keimirsan <bartek@dziak.rdk.pl>
2022-06-11 13:30:56 +0200

    Create biblioteka.sql

---
+++ Baza/biblioteka.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 11 Cze 2022, 13:29
+-- Wersja serwera: 10.4.24-MariaDB
+-- Wersja PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `biblioteka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `gatunek`
+--
+
+CREATE TABLE `gatunek` (
+  `ID` int(11) NOT NULL,
+  `gatunek` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `gatunek`
+--
+
+INSERT INTO `gatunek` (`ID`, `gatunek`) VALUES
+(1, 'Fantastyka'),
+(2, 'Sci-Fi'),
+(3, 'Romans'),
+(4, 'Horror');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ksiazki`
+--
+
+CREATE TABLE `ksiazki` (
+  `ID` int(11) NOT NULL,
+  `tytuł` text COLLATE utf8_polish_ci NOT NULL,
+  `gatunek` int(11) NOT NULL,
+  `autor` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `ksiazki`
+--
+
+INSERT INTO `ksiazki` (`ID`, `tytuł`, `gatunek`, `autor`) VALUES
+(1, 'Lew, czarownica i stara szafa', 1, 'C.S. Lewis');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `gatunek`
+--
+ALTER TABLE `gatunek`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `gatunek`
+--
+ALTER TABLE `gatunek`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
