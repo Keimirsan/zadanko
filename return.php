3512664b35f13e98d6d468fd3f5f93f8963fc6d5
Keimirsan <bartek@dziak.rdk.pl>
2022-06-13 18:48:06 +0200

    Create return.php

---
+++ return.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+     <head>
+          <meta charset="UTF-8" />
+          <title>Biblioteka</title>
+		  <link rel="stylesheet" href="style.css" type="text/css">
+     </head>
+     <body>
+		<div class="menu">
+			<ul class="ulmenu">
+				<li class="mbutton center"><a href="index.php">Strona główna</a></li>
+				<li class="mbutton center"><a href="library.php">Biblioteka</a></li>
+				<li class="mbutton center"><a href="lend.php">Wypożycz</a></li>
+				<li class="mbutton center"><a href="return.php">Zwróć</a></li>
+				<li class="mbutton center"><a href="stats.php">Statystyka</a></li>
+			</ul>
+		</div>
+		<div class="glowna">
+			
+		</div>
+     </body>
+</html>
