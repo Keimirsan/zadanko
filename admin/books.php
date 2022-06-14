7aa35cce297275b511d92f6382ead5c256d8e678
BaroshVBi <88373976+BaroshVBi@users.noreply.github.com>
2022-06-14 13:29:05 +0200

    dodawanie ksiazek

--- admin/books.php
+++ admin/books.php
@@ -17,8 +17,37 @@
 				<li class="mbutton center"><a href="logout.php"><p>Wyloguj</p></a></li>
 			</ul>
 		</div>
+					<?php
+				//include("config.php");
+			   
+				if($_SERVER["REQUEST_METHOD"] == "POST") { 
+					 
+					
+					$tytul = mysqli_real_escape_string($db,$_POST['tytul']);
+					$gatunek = mysqli_real_escape_string($db,$_POST['gatunek']);
+					$autor = mysqli_real_escape_string($db,$_POST['autor']);
+					
+				  
+					$sql = "INSERT INTO ksiazki(tytuł, gatunek, autor) VALUES ('$tytul','$gatunek','$autor')";
+					$result = mysqli_query($db,$sql);
+					echo $result;
+				}
+			?>
 		<div class="glowna">
-		
+			<form action = "" method = "post">
+				<h2>Dodaj książkę</h2>
+				<label>Tytul:</label><input type = "text" name = "tytul" class = "box"/><br /><br/>
+				<label>Gatunek:</label>
+					<select name = "gatunek">
+						<option value = "1">Fantastyka</option>
+						<option value = "2">Sci-Fi</option>
+						<option value = "3">Romans</option>
+						<option value = "4">Horror</option>
+					</select>
+				<br/><br/>
+				<label>Autor:</label><input type = "text" name = "autor" class = "box"/><br /><br/>
+				<input type = "submit" value = " Submit "/><br/>
+			</form>
 		</div>
      </body>
 </html>
