bb2a07154691f7c6b568f95763fe8e8bf080764f
BaroshVBi <88373976+BaroshVBi@users.noreply.github.com>
2022-06-14 13:09:59 +0200

    panel admin

---
+++ admin/start.php
@@ -0,0 +1,24 @@
+<?php
+	include('session.php');
+?>
+<html>
+     <head>
+          <meta charset="UTF-8" />
+          <title>Biblioteka</title>
+		  <link rel="stylesheet" href="style.css" type="text/css">
+     </head>
+     <body>
+		<div class="menu">
+			<ul class="ulmenu">
+				<li class="mbutton center"><a href="start.php"><p>Strona główna</p></a></li>
+				<li class="mbutton center"><a href="books.php"><p>Książki</p></a></li>
+				<li class="mbutton center"><a href="readers.php"><p>Czytelnicy</p></a></li>
+				<li class="mbutton center"><a href="stats.php"><p>Statystyka</p></a></li>
+				<li class="mbutton center"><a href="logout.php"><p>Wyloguj</p></a></li>
+			</ul>
+		</div>
+		<div class="glowna">
+		
+		</div>
+     </body>
+</html>
