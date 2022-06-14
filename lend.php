0215039d97a3655f3e91eb58ad2ee2a394622ac2
BaroshVBi <88373976+BaroshVBi@users.noreply.github.com>
2022-06-14 07:59:08 +0200

    Poprawienie guzików

--- lend.php
+++ lend.php
@@ -8,11 +8,11 @@
      <body>
 		<div class="menu">
 			<ul class="ulmenu">
-				<li class="mbutton center"><a href="index.php">Strona główna</a></li>
-				<li class="mbutton center"><a href="library.php">Biblioteka</a></li>
-				<li class="mbutton center"><a href="lend.php">Wypożycz</a></li>
-				<li class="mbutton center"><a href="return.php">Zwróć</a></li>
-				<li class="mbutton center"><a href="return.php">Statystyka</a></li>
+				<li class="mbutton center"><a href="index.php"><p>Strona główna</p></a></li>
+				<li class="mbutton center"><a href="library.php"><p>Biblioteka</p></a></li>
+				<li class="mbutton center"><a href="lend.php"><p>Wypożycz</p></a></li>
+				<li class="mbutton center"><a href="return.php"><p>Zwróć</p></a></li>
+				<li class="mbutton center"><a href="stats.php"><p>Statystyka</p></a></li>
 			</ul>
 		</div>
 		<div class="glowna">
