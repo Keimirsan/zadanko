6bff6aea549afd209bfa798e7c25aa6ab61cfaa4
Keimirsan <bartek@dziak.rdk.pl>
2022-06-15 14:56:36 +0200

    Zapytania o ksiązki - w trakcie

--- library.php
+++ library.php
@@ -17,27 +17,48 @@
 		</div>
 		<div class="glowna">
 		<h1>WYSZUKIWANIE KSIĄŻEK</h1>
-			
+			<form action = "library.php" method = "post">
+				<label>Tytuł:</label><input type = "text" name = "tytul" class = "box"/>
+				<label for="gatunek">Gatunek:</label>
+				<select id="gatunek" name = "gatunek">
+					<option value="Wszystkie">Wszystkie</option>
+					 <option value="Fantastyka">Fantastyka</option>
+					 <option value="Sci-Fi">Sci-Fi</option>
+					 <option value="Romans">Romans</option>
+					 <option value="Horror">Horror</option>
+				</select>
+				<input type = "submit" value = " Szukaj "/><br/>
+			</form>
+			<table>
 			<?php
-			$mysqli = new mysqli("localhost","root","","biblioteka");
-
-			// Check connection
-			if ($mysqli -> connect_errno) {
-			  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-			  exit();
-			}
+			include("config.php");
+				session_start();
+				
+				if($_SERVER["REQUEST_METHOD"] == "POST") { 
+						// username and password sent from form 
+						
+					$tytul = mysqli_real_escape_string($db,$_POST['tytul']); 
+					  
+						while ($tytul = ''){
+						$tytul = str_replace('', '*', $tytul);
+						}
+					  
+					  
+				}
+			
 			
 			mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-			$query = "SELECT id, tytuł, gatunek, autor FROM ksiazki ORDER BY ID DESC";
+			$query = "SELECT id, tytuł, gatunek, autor FROM ksiazki WHERE tytuł = '$tytul' ORDER BY ID";
 
-			$result = $mysqli->query($query);
+			$result = $db->query($query);
 			
 			/* fetch object array */
 			while ($row = $result->fetch_row()) {
 				printf("<ul>%s %s %s %s\n</ul>", $row[0], $row[1], $row[2], $row[3]);
 			}
 			?>
+			</table>
 		</div>
      </body>
 </html>
