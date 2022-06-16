76e3b87a530bf8761777d3a0e6c3a71d3f6c6f68
BaroshVBi <88373976+BaroshVBi@users.noreply.github.com>
2022-06-16 14:48:57 +0200

    Update adduser.php

--- admin/adduser.php
+++ admin/adduser.php
@@ -3,9 +3,11 @@
 	if($_SERVER["REQUEST_METHOD"] == "POST") { 
 		$login = mysqli_real_escape_string($db,$_POST['login']);
 		$sqlCheck = mysqli_query($db,"select login from czytelnicy where czytelnicy.login = '$login' ");
-		mysqli_fetch_array($sqlCheck,MYSQLI_ASSOC);
-		//echo count($row);
-		if(count($row) == 0){
+		$row = mysqli_fetch_array($sqlCheck,MYSQLI_ASSOC);
+		//echo $row['login'];
+		if(is_countable($row)){
+			echo "uzytkownik z podanym loginem juz istnieje";
+		}else{
 			$haslo = mysqli_real_escape_string($db,$_POST['haslo']);
 			$imie = mysqli_real_escape_string($db,$_POST['imie']);
 			$urodziny = mysqli_real_escape_string($db,$_POST['urodziny']);
@@ -13,8 +15,6 @@
 			$sql = "INSERT INTO czytelnicy(login, password, imie, urodziny, plec) VALUES ('$login','$haslo','$imie', '$urodziny', '$plec')";
 			$result = mysqli_query($db,$sql);
 			header("location:readers.php");
-		}else{
-			echo "uzytkownik z podanym loginem juz istnieje";
 		}
 	}
 ?>
